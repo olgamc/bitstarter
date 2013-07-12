cb9557e906fc471bbe28861dca74b7d5afd7508a
Olga McCarthy <olgamccarthy@yahoo.ca>
2013-07-12 16:32:32 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  var str = buffer.toString('utf-8');
+  console.log(str);
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
